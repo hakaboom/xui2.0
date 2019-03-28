7a30d2ffc01d7c7e0dd05dd1128ba6d8d2980ffd
hakaboom <270704881@qq.com>
2019-03-28 23:49:47 +0800

    stepper

--- xsp/src/main.lua
+++ xsp/src/main.lua
@@ -4,14 +4,18 @@ require'base'
 setmetatable(_ENV,{__index=require'xui.xui'})
 show=true
 ui=rootView:createLayout({Area=Rect(100,100,1280,720),Color='rgba(255,255,255,0.1)'})
-ui:creatContext()
+context=ui:creatContext()
+
+callPrint = function (base)
+	local view = base.view
+	Print(base)
+end
+
+lable=layout:createLayout({id='lable',ui=ui,w=100,h=90}):addToRootView()
 
 
-lable=layout:createLayout({ui=ui,w=100,h=90}):addToRootView()
-
-
-page1=lable:createLayout({w=100,h=70}):addToSubview()
-page2=lable:createLayout({w=100,h=70}):addToSubview()
+page1=lable:createLayout({w=100,h=60,Color='red'}):addToSubview()
+--page2=lable:createLayout({w=100,h=70}):addToSubview()
 a=page1:createTabPage({
 	list={
 		{value='1'},{value='2'},
@@ -25,12 +29,12 @@ a=page1:createTabPage({
 	tabStyle = {
 		backgroundColor = 'black'
 	},
-}):addToSubview():setActionCallback(function(Base) end)
+}):addToSubview():setActionCallback(function(Base) print(Base.layoutView) end)
 
 
 
 title=layout:createLayout({ui=ui,w=100,h=10,sort='row',Color='blue'}):addToRootView()
-b=title:createButton({w=20,text="close"}):addToSubview():setActionCallback(function()print(1) end)
+b=title:createStepper({w=20,number=11,maximum=13,step=2}):addToSubview():setActionCallback(function()print(1) end)
 title:createInput({w=40,prompt="start"}):addToSubview():setActionCallback( function(Base)
 end)
 
@@ -40,22 +44,8 @@ end)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ui:show()
+
 while show do
 	sleep(1000)
 end
--- xsp/src/xui/xui.lua
+++ xsp/src/xui/xui.lua
@@ -119,6 +119,38 @@ end
 function class:getType()
 	return self.__tag
 end
+
+local _storage={}
+function _storage:new(fileName)
+	local o ={
+		data={},
+		path = xmod.resolvePath('[private]'..(fileName or 'UISave.txt'))
+	}
+	
+	local cjson = require'cjson'
+	local file = io.open(o.path,'a+')
+	
+	local str = file:read('a')
+	file:close()
+	if #str~=0 then
+		o.data = cjson.encode(str)
+	end
+	
+	setmetatable(o,{__index = _storage})
+	return o
+end
+function _storage:put(key,value)
+	self.data[key] = value
+end
+function _storage:get(key,defValue)
+	return (self.data[key] or defsValue)
+end
+function _storage:save()
+	local cjson = require'cjson'
+	local t = self.data
+	local str = cjson.decode(t)
+	io.open(o.path,'r+'):write(ste):flush():close()
+end
 ------------------------------------------------------------------
 
 local _rootView={}
@@ -161,7 +193,7 @@ end
 ------------------------------------------------------------------
 local _layout=class:new()
 local xui_layout={
-	Count = 0,	
+	Count = 0,
 }
 function _layout:createLayout(Base)
 	local Base = Base or {}
@@ -172,7 +204,7 @@ function _layout:createLayout(Base)
 	if not Base.id then
 		xui_layout.Count = xui_layout.Count + 1
 	end
-	
+
 	local o = {
 		__tag = 'layout',
 		context = self.context or Base.ui.context,
@@ -190,9 +222,10 @@ function _layout:createLayout(Base)
 				left = xpos,
 				top = ypos,
 				backgroundColor = Base.Color or 'rgb(255,255,255)',
-				['flex-direction'] = Base.sort or 'column'
+				['flex-direction'] = Base.sort or 'column',
 			},
 			subviews = {},
+			['scroll-direction'] = (Base.sort == 'row') and 'horizontal' or 'vertical',
 		},
 	}
 	
@@ -209,6 +242,7 @@ function _layout:setActionCallback(callback)
 		end
 	end
 	view:setActionCallback(UI.ACTION.CLICK, onClicked)
+	view:setActionCallback(UI.ACTION.SWIPE, onClicked)
 	return self
 end
 
@@ -241,8 +275,8 @@ function _button:createLayout(Base)
 	local Base = Base or {}
 	local xpos = Base.xpos and Base.xpos/100*self.width or 0
 	local ypos = Base.ypos and Base.ypos/100*self.height or 0
-	local width = (Base.w or 100)/100*self.width
-	local height = (Base.h or 100)/100*self.height
+	local width = math.floor((Base.w or 100) /100*(self.width or ui.width))
+	local height = math.floor((Base.h or 100) /100*(self.height or ui.height))
 	if not Base.id then
 		xui_button.Count = xui_button.Count + 1
 	end
@@ -258,7 +292,7 @@ function _button:createLayout(Base)
 	}
 	local style = Base.style or {}
 	local fontSize = (style.fontSize or style['font-size']) or 18
-	local textColor = style.textColor or '#000000'	
+	local textColor = style.textColor or '#333333'	
 	local backgroundColor = (style.backgroundColor or style['background-color']) or '#ffffff'
 	local checkedBackgroundColor = style.checkedBackgroundColor or '#efeff0'
 	local borderRadius = (style.borderRadius or  style['border-radius']) or 15
@@ -283,6 +317,7 @@ function _button:createLayout(Base)
 	layout.subviews[1].value = Base.text
 
 	o.con = layout
+	
 	setmetatable(o,{__index = _button})
 	return o
 end
@@ -330,12 +365,11 @@ function _input:createLayout(Base)
 	if not Base.id then
 		xui_input.Count = xui_input.Count + 1
 	end
-
+	
 	local o ={
 		__tag = 'input',
 		context = self.context,
 		parentView = self.layoutView,
-		saveData = {},
 		width = width,
 		height = height,
 		con = {
@@ -344,30 +378,35 @@ function _input:createLayout(Base)
 				width = width,
 				height = height,
 				['flex-direction'] = 'row',
-				backgroundColor = '#e5e5e5',
 				['border-radius'] = 5,
 				['align-items'] = 'center',
 			},
 			subviews = {},
 		},
 	}
-
+	
 	local id = utils.buildID('input',(Base.id or xui_button.Count))
 	local kbtype = Base.kbtype or 'text'
 	local prompt = Base.prompt or ''
 	local placeholder = Base.placeholder or ''
 	local disabled = Base.disabled or false
 	local autofocus = Base.autofocus or false
-	local maxlength = Base.maxlength or 1
+	local maxlength = Base.maxlength or 999
 	local singleline = Base.singleline or true
 
 	local style = Base.style or {}
 	local fontSize = (style.fontSize or style['font-size']) or 20
-	local textColor = style.textColor or '#ababab'
-	local backgroundColor = (style.backgroundColor or style['background-color']) or '#000000'
+	local textColor = style.textColor or '#666666'
+	local backgroundColor = (style.backgroundColor or style['background-color']) or '#e5e5e5'
 	local checkedBackgroundColor = style.checkedBackgroundColor or '#000000'
 	local layout = xui_input.layout()
 	
+	local cancelStyle = style.cancelStyle or {}
+	local cancelFontSize = cancelStyle.fontSize or 15
+	local cancelBackgroundColor = cancelStyle.backgroundColor or '#eeeeee'
+	local cancelCheckedBackgroundColor = cancelStyle.cancelCheckedBackgroundColor or cancelBackgroundColor
+	
+	o.con.style.backgroundColor = backgroundColor
 	local inputLayout = {
 		view = 'input',
 		type = kbtype,
@@ -375,7 +414,7 @@ function _input:createLayout(Base)
 		placeholder = placeholder,
 		disabled = disabled,
 		autofocus = autofocus,
-		maxlength = maxlengthm,
+		maxlength = maxlength,
 		singleline = singleline,
 		style = {
 			width = width,
@@ -388,10 +427,10 @@ function _input:createLayout(Base)
 	o.con.subviews[1] = inputLayout
 	
 	local cancel = self.createButton(o,{w=20,text='取消',style={
-		backgroundColor = '#eeeeee',
-		fontSize = 15,
+		backgroundColor = cancelBackgroundColor,
+		fontSize = cancelFontSize,
 		borderRadius = 0,
-		checkedBackgroundColor = 'red'
+		checkedBackgroundColor = cancelCheckedBackgroundColor,
 	}})
 	cancel:setStyle({
 		left = width*0.8,
@@ -419,7 +458,7 @@ function _input:setActionCallback(callback)
 	local onINPUT = function (id,action)
 		local value = view:getAttr('value')
 		local Base = {id=id,action=action,view=view,value=value}
-		self.saveData.value = value
+		
 		if callback then
 			callback(Base)
 		end
@@ -427,7 +466,7 @@ function _input:setActionCallback(callback)
 	
 	cancel:setActionCallback(UI.ACTION.CLICK,onCancel)
 	inputView:setActionCallback(UI.ACTION.CLICK,onClicked)
-	view:setActionCallback(UI.ACTION.INPUT,onINPUT)
+	inputView:setActionCallback(UI.ACTION.INPUT,onINPUT)
 end
 function _input:getValue()
 	return self.saveData.value
@@ -495,6 +534,146 @@ function _gridSelect:createLayout(Base)
 
 end
 ------------------------------------------------------------------
+local _stepper=class:new()
+local xui_stepper={
+	Count = 0,
+	layout = function ()
+		return {
+			{
+				view = 'div',
+				style = {
+					flex = 1,
+					['align-items'] = 'center',
+				},
+				subviews = {
+					{
+						view = 'text',
+						value = '-',
+						style = {},
+					},
+				},
+			},
+			{
+				view = 'input',
+				type = 'number',
+				style = {
+					flex = 2,
+					['text-align'] = 'center',
+				},
+				value = '',
+				singleline = true,
+				disabled = true,
+			},
+			{
+				view = 'div',
+				style = {
+					flex = 1,
+					['align-items'] = 'center',
+				},
+				subviews = {
+					{
+						view = 'text',
+						value = '+',
+						style = {},
+					},					
+				},
+			},
+		}
+	end
+}
+function _stepper:createLayout(Base)
+	local Base = Base or {}
+	local xpos = Base.xpos and Base.xpos/100*self.width or 0
+	local ypos = Base.ypos and Base.ypos/100*self.height or 0
+	local width = math.floor((Base.w or 100) /100*(self.width or ui.width))
+	local height = math.floor((Base.h or 100) /100*(self.height or ui.height))
+	
+	local value = tonumber(Base.number) or 0
+	local step = tonumber(Base.step) or 1
+	local maximum = Base.maximum or 999
+	local minimum = Base.minimum or 0
+	local maxlength = Base.maxlength or #tostring(maximum)
+	local style = Base.style or {}
+	local fontSize = (style.fontSize or style['font-size']) or 18
+	local backgroundColor = (style['background-color'] or style.backgroundColor) or '#ffffff'
+	
+	local buttonStyle = style.buttonStyle or {}
+	local buttonBackgroundColor = buttonStyle.backgroundColor or '#e5e5e5'
+	local buttonFontSize = (buttonStyle.fontSize or buttonStyle['font-size']) or 20
+		
+	local o = {
+		__tag = 'stepper',
+		context = self.context,
+		parentView = self.layoutView,
+		config = {maximum= maximum,minimum = minimum,step = step},
+		width = width,
+		height = height,
+		con = {
+			view = 'div',
+			style = {
+				['flex-start'] = 'space-between',
+				['align-items'] = 'center',
+				['flex-direction'] = 'row',
+				width = width,
+				height = height,
+				backgroundColor = backgroundColor,
+			},
+			subviews = {},
+		},
+	}
+	
+	local layout = xui_stepper.layout()
+	o.con.subviews = layout
+	
+	local inputView = layout[2]
+	local inputStyle = inputView.style
+	inputStyle.fontSize = fontSize
+	inputStyle.maxlength = maxlength
+	inputView.value = value
+	
+	for k,v in ipairs({1,3}) do
+		local view = layout[v]
+		local viewStyle = view.style
+		viewStyle.backgroundColor = buttonBackgroundColor 
+		
+		local textView = view.subviews[1]
+		local textStyle = textView.style
+		textStyle.fontSize = buttonFontSize
+	end
+	setmetatable(o,{__index = _stepper})
+	return o
+end
+function _stepper:setActionCallback(callback)
+	local view = self.layoutView
+	local addView = view:getSubview(3)
+	local textView = view:getSubview(2)
+	local reduceView = view:getSubview(1)
+	
+	local maximum = self.config.maximum
+	local minimum = self.config.minimum
+	local step = self.config.step
+	
+	local onAdd = function ()
+		local num = tonumber(textView:getAttr('value'))
+		if num+step <= maximum then
+			textView:setAttr('value',num+step)
+		end
+	end
+	local onReduce = function ()
+		local num = tonumber(textView:getAttr('value'))
+		if num-step >= minimum then
+			textView:setAttr('value',num-step)	
+		end
+	end
+	
+	addView:setActionCallback(UI.ACTION.CLICK, onAdd)
+	reduceView:setActionCallback(UI.ACTION.CLICK, onReduce)
+	return self
+end
+function _layout:createStepper(Base)
+	return _stepper.createLayout(self,Base)
+end
+------------------------------------------------------------------
 local _tabPage=class:new()
 local xui_tabPage={
 	Count=0,
@@ -537,7 +716,12 @@ local xui_tabPage={
 			subviews = {},
 		}
 	end,
-	config={},
+	toString = function (Base)
+		local str = string.format('tabPage<id=%s,tabCount=%s,width=%s,height=%s>',
+			Base.con.id,#Base.config.pages,Base.width,Base.height
+		)
+		return str
+	end,
 } 
 function _tabPage:buildTitle(list,style)
 	local o={
@@ -549,8 +733,8 @@ function _tabPage:buildTitle(list,style)
 	local list = list or {}
 	local style = style or {}
 	local layoutSort = self.con.style['flex-direction']
-	local width  =	layoutSort =='column' and self.width or self.width*(style.w or 12)/100
-	local height =	layoutSort =='column' and self.height*(style.h or 15)/100 or self.height
+	local width  =	math.floor(layoutSort =='column' and self.width or self.width*(style.w or 12)/100)
+	local height =	math.floor(layoutSort =='column' and self.height*(style.h or 15)/100 or self.height)
 	
 	local titleWidth  = layoutSort =='column' and width*(style.titleWidth or 15)/100 or width
 	local titleHeight = layoutSort =='column' and height or height*(style.titleHeight or 15)/100 
@@ -635,30 +819,31 @@ function _tabPage:buildTab(list,style)
 	o.subviews[1].style.height = height
 
 	for i=1, #list do
-	  local tabView = _layout:createLayout({ui=self,id=utils.buildID(self.con.id..'_tab',list[i].value)})
+		local value = list[i].value
+		local tabView = _layout:createLayout({ui=self,id=utils.buildID(self.con.id..'_tab',value)})
 			tabView:setStyle({
 				width=width,
 				height=height,
 				backgroundColor = backgroundColor,
 			})
 			
-			if type(list[i].tabStyle) =='table' then
-				tabView:setStyle(list[i].tabStyle)
-			end
+		if type(list[i].tabStyle) =='table' then
+			tabView:setStyle(list[i].tabStyle)
+		end
 			
-			self.config.pages[i] = tabView
-			o.subviews[1].subviews[i] = tabView:getView()
+		self.config.pages[i] = tabView
+		o.subviews[1].subviews[i] = tabView:getView()
 	end
 	
 	return o
 end
 function _tabPage:createLayout(Base)
-	local	Base = Base or {}
-	local	xpos = Base.xpos and Base.xpos/100*self.width or 0
-	local	ypos = Base.ypos and Base.ypos/100*self.height or 0
-	local	layoutSort = Base.sort=='row' and 'row' or 'column'
-	local	width = math.floor((Base.w or 100) /100*(self.width or ui.width))
-	local	height = math.floor((Base.h or 100) /100*(self.height or ui.height))
+	local Base = Base or {}
+	local xpos = Base.xpos and Base.xpos/100*self.width or 0
+	local ypos = Base.ypos and Base.ypos/100*self.height or 0
+	local layoutSort = Base.sort=='row' and 'row' or 'column'
+	local width = math.floor((Base.w or 100) /100*(self.width or ui.width))
+	local height = math.floor((Base.h or 100) /100*(self.height or ui.height))
 	if not Base.id then
 		xui_tabPage.Count = xui_tabPage.Count + 1
 	end
@@ -686,9 +871,9 @@ function _tabPage:createLayout(Base)
 		},
 	}
 
-	local	list = Base.list or {}
-	local	titleStyle = Base.titleStyle or {}
-	local 	tabStyle = Base.tabStyle or {}
+	local list = Base.list or {}
+	local titleStyle = Base.titleStyle or {}
+	local  tabStyle = Base.tabStyle or {}
 
 	local titleView = _tabPage.buildTitle(o,list,titleStyle)
 	local titleWidth = titleView.style.width
@@ -703,15 +888,15 @@ function _tabPage:createLayout(Base)
 	o.tabWidth = tabStyle.width
 	o.tabHeight = tabStyle.height
 
-	setmetatable(o,{__index = _tabPage})
+	setmetatable(o,{__index = _tabPage,__tostring = xui_tabPage.toString})
 	return o
 end
 function _tabPage:createView()
 	local context = self.context
 	local view = context:createView(self.con)
-
+	
 	self.layoutView = view
-
+	
 	local tabView = view:getSubview(2):getSubview(1)
 	local tabCount = tabView:subviewsCount()
 	for i = 1, tabCount do
